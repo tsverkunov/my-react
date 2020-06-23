0e8a3d8fd77e72cb5d60fdef21e253077fcb4f45
tsverkunov <61116987+tsverkunov@users.noreply.github.com>
2020-06-23 12:39:53 +0300

    Updates

---
+++ precache-manifest.c4e4c0265ec7ac96f3e7c8b23ba252ae.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67ba27a17cb0e3f36cccda081a110955",
+    "url": "/my-react/index.html"
+  },
+  {
+    "revision": "3f0ca27517f13fe85dbe",
+    "url": "/my-react/static/css/3.8816ad8f.chunk.css"
+  },
+  {
+    "revision": "2763292cb2da7ce15384",
+    "url": "/my-react/static/css/main.9fcf0f61.chunk.css"
+  },
+  {
+    "revision": "37e074f55f4606081904",
+    "url": "/my-react/static/js/2.43e48a11.chunk.js"
+  },
+  {
+    "revision": "cacaf3f01dd3bd8237006f7c58951c1c",
+    "url": "/my-react/static/js/2.43e48a11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f0ca27517f13fe85dbe",
+    "url": "/my-react/static/js/3.0ceb84b7.chunk.js"
+  },
+  {
+    "revision": "4d14c5620b413e6dc117",
+    "url": "/my-react/static/js/4.cb69b507.chunk.js"
+  },
+  {
+    "revision": "2763292cb2da7ce15384",
+    "url": "/my-react/static/js/main.b66df3e2.chunk.js"
+  },
+  {
+    "revision": "5ab01889e4cfe5c2e4a0",
+    "url": "/my-react/static/js/runtime-main.83b81e11.js"
+  },
+  {
+    "revision": "7909d125889929f378f1d3ed76fdad2f",
+    "url": "/my-react/static/media/EXAH-business-black.7909d125.svg"
+  },
+  {
+    "revision": "45a0ba998010121c05fe89dea34a71ea",
+    "url": "/my-react/static/media/back.45a0ba99.svg"
+  },
+  {
+    "revision": "3e613d2b539a02ea0687fc1aacbc0092",
+    "url": "/my-react/static/media/editing_icon.3e613d2b.svg"
+  },
+  {
+    "revision": "1f6e9cb7f5ae545c75a65def09ac06b2",
+    "url": "/my-react/static/media/like_icon.1f6e9cb7.svg"
+  },
+  {
+    "revision": "98f23b89a8610f62dc16efcd27da7b5b",
+    "url": "/my-react/static/media/next.98f23b89.svg"
+  },
+  {
+    "revision": "b9a5a2c3f709285e86529e63fb6fb245",
+    "url": "/my-react/static/media/no.b9a5a2c3.png"
+  },
+  {
+    "revision": "5387dd04f91043325e43d3631c2a9921",
+    "url": "/my-react/static/media/preloader_100px_BGgrey.5387dd04.svg"
+  },
+  {
+    "revision": "0e78e46a27673bf36873404a8e9dc050",
+    "url": "/my-react/static/media/preloder_ball_40px.0e78e46a.svg"
+  },
+  {
+    "revision": "76f75ac9c4c7de40f0c8ab84fa015304",
+    "url": "/my-react/static/media/users_icon.76f75ac9.png"
+  },
+  {
+    "revision": "c1b74d1ed8ee8c441663a99cf89eb275",
+    "url": "/my-react/static/media/yes.c1b74d1e.png"
+  }
+]);
